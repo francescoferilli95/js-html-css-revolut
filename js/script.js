008886f86f7f1cc8c1bf1844e9ef2015aac3118d
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-04-09 16:48:01 +0200

    adding script js to toggle menus on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    
+
+    /**
+     * 
+     *  DROPDOWN MENUS
+     * 
+    /**/
+
+    // REFS
+    var dropDown = $('.with-dropdown');
+    var dropLink = dropDown.children('a');
+    var dropMenu = dropDown.children('.dropdown-menu');
+
+    // CLICK EVENT
+    dropLink.click(function() {
+        // MENU ACTIVE DURING CLICK EVENT
+        var actualMenu = $(this).next('.dropdown-menu');
+
+        dropMenu.not(actualMenu).hide();
+        actualMenu.toggle();
+    });
+
+
+// END OF DOC READY
 });
